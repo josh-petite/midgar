ef608ff0caa1461c5521f548ee2e1eed42a1479b
Josh Petite <josh.petite@gmail.com>
2020-05-23 22:24:32 -0700

    Fixing repo

--- CreateProjects.bat
+++ CreateProjects.bat
@@ -1,2 +1,5 @@
+if not exist ".\Midgar\vendor\GLFW" mkdir ".\Midgar\vendor\GLFW"
+if not exist ".\Midgar\vendor\spdlog" mkdir ".\Midgar\vendor\spdlog"
+
 call vendor\bin\premake\premake5.exe vs2019
 PAUSE
